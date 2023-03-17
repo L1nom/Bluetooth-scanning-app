ea992593c1133757a9f59799d863f5a68a5643fb
L1nom <monilp01@gmail.com>
2023-03-17 03:41:22 -0400

    Added code to add exe to registry for open with url protocol

--- gui.py
+++ gui.py
@@ -4,6 +4,8 @@ from bleak import BleakScanner
 import requests
 import json
 import threading
+import winreg
+import os
 from location import trilateration
 import sys
 
@@ -17,10 +19,30 @@ device_list = []
 token = None
 student_id_encrypted = None
 
-test_1 = sys.argv[1]
-test_2 = sys.argv[2]
+test_1 = sys.argv[1]  # arg 1 would be student header
+test_2 = sys.argv[2]  # arg 2 would be the course id
 
 print(test_1, test_2)
+
+
+file_path = "\"" + os.path.abspath(os.path.basename(__file__)) + "\"" + " \"%1\","
+path = winreg.HKEY_CLASSES_ROOT
+try:
+    with winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT) as hkey:
+        winreg.CreateKey(hkey, "Locatify")
+    with winreg.OpenKey(path, r"Locatify\\", 0, winreg.KEY_ALL_ACCESS) as locatify:
+        winreg.SetValueEx(locatify, "URL Protocol", 0, winreg.REG_SZ, "url protocol")
+        winreg.CreateKey(locatify, "shell")
+        with winreg.OpenKeyEx(path, r"Locatify\\shell", 0, winreg.KEY_ALL_ACCESS) as shell:
+            winreg.CreateKey(shell, "open")
+            with winreg.OpenKeyEx(path, r"Locatify\\shell\\open", 0, winreg.KEY_ALL_ACCESS) as open:
+                winreg.CreateKey(open, "command")
+                with winreg.OpenKeyEx(path, r"Locatify\\shell\\open\\command", 0, winreg.KEY_ALL_ACCESS) as command:
+                    winreg.SetValueEx(command, None, 0, winreg.REG_SZ, "\"C:\Windows\System32\calc.exe\" \"%1\",")
+                    winreg.SetValueEx(command, "test", 0, winreg.REG_SZ, file_path)
+
+except PermissionError:
+    pass
 
 
 async def main():
