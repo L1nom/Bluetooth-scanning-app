7289e79b52fae12b6d00e9d6b8c1a469fa5318ed
L1nom <monilp01@gmail.com>
2023-04-02 17:45:22 -0400

    made scan continuous

--- gui.py
+++ gui.py
@@ -1,3 +1,6 @@
+import threading
+import asyncio
+import tkinter as tk
 import customtkinter
 import asyncio
 from bleak import BleakScanner
@@ -5,82 +8,128 @@ import requests
 import json
 import threading
 from location import trilateration
-import sys
+import threading
+import os
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+# Define our constants
 
-login_frame = customtkinter.CTk()
-login_frame.geometry("600x350")
+root = customtkinter.CTk()
+root.geometry("600x350")
 
-device_list = []
-token = None
-student_id_encrypted = None
+destroy = False
 
 # test = sys.argv[1]
+test = "zlocatify:?token=\"aqqweqweqwwe\"?course_id=\"qweqeqwqwe\"?session_id=\"qweqeqwqwe\"?student_id=\"qweqeqwqwe\""
+splits = test.split('?')
+if splits[1]:
+    token = splits[1]
+    token = token.split('=')[1]
+if splits[2]:
+    course_id = splits[2]
+    course_id = course_id.split('=')[1]
+if splits[3]:
+    session_id = splits[3]
+    session_id = session_id.split('=')[1]
+if splits[3]:
+    student_id = splits[3]
+    student_id = student_id.split('=')[1]
+print(token, course_id, session_id, student_id)
 
 
-# test = "zlocatify:?header=\"aqqweqweqwwe\"?course_id=\"qweqeqwqwe\""
-# print(test)
-# splits = test.split('?')
-# header = splits[1]
-# header = header.split('=')[1]
-# course_id = splits[2]
-# course_id = course_id.split('=')[1]
-# print(header, course_id)
+def join_session():
+    url = "https://capstonebackend.fly.dev/join/session"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token
+    }
+
+    payload = json.dumps({
+                "course_id": course_id,
+                "student_id ": student_id
+            })
+    
+    requests.post(url, headers=headers, data=payload)
 
 
-async def main():
-    devices = await BleakScanner.discover(timeout=10.0)
-    for d in devices:
-        device_list.append([d.address, d.rssi])
+async def scanning_task():
+    url = "https://capstonebackend.fly.dev/set/student_position"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token
+    }
+
+    while True:
+
+        scanning_message.pack()
+        progress_bar.pack(pady=12, padx=10)
+        progress_bar.start()
+        progress_bar.step()
+
+        devices = await BleakScanner.discover(timeout=10.0)
+        device_list = []
+        for d in devices:
+            device_list.append([d.address, d.rssi])
+        print("Done")
+
+        await asyncio.sleep(1)
+
+        if destroy:
+            try:
+                os._exit(1)
+
+            except Exception:
+                os._exit(1)
+                
+        scanning_message.forget()
+        scanning_success.pack()
+        scanning_success.after(3000, scanning_success.forget)
+        progress_bar.stop()
+        progress_bar.forget()
+
+        if device_list:
+            # location_x, location_y = trilateration(beacon_location_list, rssi_list)
+            location_x, location_y = 300, 400
+            payload = json.dumps({
+                "course_id": course_id,
+                "x_position": location_x,
+                "y_position": location_y
+            })
+            location_x += 100
+            location_y += 100
+            response = requests.post(url, headers=headers, data=payload)
+
+            if response.status_code == 200:
+                print('pushed')
+            if response.status_code != 200:
+                error_message.pack()
+                error_message.after(3000, error_message.forget)
+
+        # Wait for 1 minute before running again
+        await asyncio.sleep(10)
 
 
-def login():
-    student_id = entry1.get().strip()
-    student_password = entry2.get().strip()
-
-    if student_id and student_password:
+def start_scanning_task():
+    # Start the event loop in a new thread
+    global loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(scanning_task())
+    try:
+        loop.run_forever()
+    finally:
         try:
-            response_code = validate_login(student_id, student_password)
-            if response_code == 200:
-                entry1.destroy()
-                entry2.destroy()
-                button.destroy()
-                button1.pack(pady=12, padx=10)
-                button2.pack(pady=12, padx=10)
+            
+            loop.run_until_complete(loop.shutdown_asyncgens())
+            loop.close()
+            root.destroy()
+            os._exit(1)
 
-        except ValueError:
-            pass
+        except Exception:
+            os._exit(1)
 
 
-def validate_login(student_id, password):
-    global token
-    global student_id_encrypted
-    url = 'https://capstonebackend.fly.dev/auth/signin'
-    header = {
-        'Content-Type': 'application/json'}
-    payload_data = json.dumps({
-        "password": password,
-        "studentID": student_id
-    })
-
-    response = requests.post(url, headers=header, data=payload_data)
-
-    if response.status_code == 200:
-        login_success_message.pack()
-        login_success_message.after(3000, login_success_message.destroy)
-        response_dictionary = json.loads(response.text)
-        token = response_dictionary['token']
-        student_id_encrypted = response_dictionary['user']['_id']
-        print(token)
-    else:
-        login_error_message.pack()
-        login_error_message.after(3000, login_error_message.destroy)
-    return response.status_code
-
-
-def raise_hand():
+def on_raise_hand():
+    print("Hello")
     url = "https://capstonebackend.fly.dev/raise_hand"
     headers = {
         'Content-Type': 'application/json',
@@ -88,8 +137,8 @@ def raise_hand():
     }
 
     payload = json.dumps({
-        "course_id": "63e3e57dc347381e72c419e5",
-        "student_id": student_id_encrypted,
+        "course_id": course_id,
+        "student_id": student_id,
         "hand_raised": True
     })
 
@@ -97,68 +146,35 @@ def raise_hand():
     print(response.status_code)
 
 
-def scan():
-    # print(token)
-    url = "https://capstonebackend.fly.dev/set/student_position"
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': token
-    }
 
-    scanning_message.pack()
-    progress_bar.pack(pady=12, padx=10)
-    progress_bar.start()
-    progress_bar.step()
-    asyncio.run(main())
-    scanning_message.destroy()
-    scanning_success.pack()
-    scanning_success.after(3000, scanning_success.destroy)
-    progress_bar.stop()
-    progress_bar.destroy()
+def on_closing():
+    global destroy
+    root.destroy()
+    loop.stop()
+    os._exit(1)
 
-    if device_list:
-        # location_x, location_y = trilateration(beacon_location_list, rssi_list)
-        location_x, location_y = 150, 225
-        payload = json.dumps({
-            "course_id": "63e3e57dc347381e72c419e5",
-            "x_position": location_x,
-            "y_position": location_y
-        })
-
-        response = requests.post(url, headers=headers, data=payload)
-
-        if response.status_code == 200:
-            button1.destroy()
-        else:
-            error_message.pack()
-            error_message.after(3000, error_message.destroy)
-            print(response)
+root.protocol("WM_DELETE_WINDOW", on_closing)
 
 
-label = customtkinter.CTkLabel(master=login_frame, text="Login", font=("Roboto", 24))
+# Create a Tkinter UI elements
+label = customtkinter.CTkLabel(master=root, text="Smart LMS", font=("Roboto", 24))
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master=login_frame, placeholder_text="Student ID")
-entry1.pack(pady=12, padx=10)
+raise_button = customtkinter.CTkButton(root, text="Raise", command=on_raise_hand)
+raise_button.pack()
 
-entry2 = customtkinter.CTkEntry(master=login_frame, placeholder_text="Password")
-entry2.pack(pady=12, padx=10)
-
-button = customtkinter.CTkButton(master=login_frame, text="Login", command=login)
-button.pack(pady=12, padx=10)
-
-button1 = customtkinter.CTkButton(master=login_frame, text="Scan", command=threading.Thread(target=scan).start)
-button2 = customtkinter.CTkButton(master=login_frame, text="Raise Hand", command=raise_hand)
-
-login_error_message = customtkinter.CTkLabel(master=login_frame,
-                                             text="Login Error!\nPlease make sure you are using a valid ID and password")
-login_success_message = customtkinter.CTkLabel(master=login_frame, text="Succesfully logged in!")
-scanning_message = customtkinter.CTkLabel(master=login_frame, text="Scanning...")
-scanning_success = customtkinter.CTkLabel(master=login_frame, text="Finished scanning!")
-error_message = customtkinter.CTkLabel(master=login_frame,
+scanning_message = customtkinter.CTkLabel(master=root, text="Scanning...")
+scanning_success = customtkinter.CTkLabel(master=root, text="Finished scanning!")
+error_message = customtkinter.CTkLabel(master=root,
                                        text="Scan error! Position not found. \n Please check Bluetooth, Internet, "
                                             "and an active class are available")
 
-progress_bar = customtkinter.CTkProgressBar(login_frame, orientation='horizontal', mode='indeterminate')
+progress_bar = customtkinter.CTkProgressBar(root, orientation='horizontal', mode='indeterminate')
 
-login_frame.mainloop()
+
+# Start the background task in a new thread
+thread = threading.Thread(target=start_scanning_task)
+thread.start()
+
+# Start the Tkinter event loop on the main thread
+root.mainloop()
